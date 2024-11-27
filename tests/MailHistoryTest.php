67580b5d3517c615dec99a993b253265be931370
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-11-27 09:30:49 +0800

    Setup Workbench and Fix Test

--- tests/MailHistoryTest.php
+++ tests/MailHistoryTest.php
@@ -4,17 +4,31 @@ use Illuminate\Mail\Events\MessageSending;
 use Illuminate\Mail\Events\MessageSent;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\DB;
 
 beforeEach(function () {
+    // Set view paths
     config()->set('view.paths', [
         dirname(__FILE__).'/resources/views',
     ]);
 
+    // Commit any existing transactions
+    try {
+        DB::commit();
+    } catch (\Exception $e) {
+        // Ignore any commit errors since there might not be an active transaction
+    }
+
+    // Run migrations
     Artisan::call('vendor:publish', [
         '--tag' => 'mailhistory-migrations',
         '--force' => true,
     ]);
+
+    // Fresh migration without transaction wrapping
+    DB::unprepared('PRAGMA foreign_keys = OFF;');
     Artisan::call('migrate:fresh');
+    DB::unprepared('PRAGMA foreign_keys = ON;');
 });
 
 it('has MessageSending and MessageSent event listened to if the package is enabled', function () {
@@ -28,7 +42,6 @@ it('has MessageSending and MessageSent event listened to if the package is enabl
 });
 
 it('does not has the MessageSending and MessageSent event listened to if the package is disabled', function () {
-
     config([
         'mailhistory.enabled' => false,
     ]);
